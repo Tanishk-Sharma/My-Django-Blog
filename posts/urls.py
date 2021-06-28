38673ee5c6bc2c1007f72d3429afb9b896e6057e
Tanishk Sharma <tanishk9597@gmail.com>
2021-06-28 13:23:34 +0530

    created few templates

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('all', views.all_posts, name='all_posts'),
+    path('view/test_post', views.view_post_test, name='view_post'),
+
 ]
 
 
--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def all_posts(request):
     return render(request, 'all_posts.html')
+
+def view_post_test(request):
+    return render(request, 'view_post.html')
